61b9a1101b88bdf1e3da3f261daf472fa7ed2cb1
Ayush K M <kmayushkm@gmail.com>
2021-09-04 18:29:32 +0530

    Uploaded encrypt_string.py

--- test/encrypt_string.py
+++ test/encrypt_string.py
@@ -1,1 +1,16 @@
+import simplyencrypt as en
 
+keys = en.new_random_password(1024)
+line = "Let's encrypt this link"
+print("Unenceypted line: " + line)
+
+cipher_text = en.encrypt(keys, line)
+try:
+	print("Encrypted line: " + str(cipher_text))
+except:
+	print("Encrypted: ", end=" ")
+	for c in cipher_text:
+		c = str(ord(c))
+		print(c, end=", ")
+	print("\nPrinted Encrypted line in termes of ord values cause the \
+values couldn't be printed as utf-8 chars")
