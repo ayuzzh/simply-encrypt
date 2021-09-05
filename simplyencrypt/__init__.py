02759a2eb16b19b5abee1fbe41687609ec30a32b
Ayush K M <kmayushkm@gmail.com>
2021-09-05 14:11:17 +0530

    Update __init__.py

--- simplyencrypt/__init__.py
+++ simplyencrypt/__init__.py
@@ -2,7 +2,7 @@
 from .a import new_random_password, encrypt
 
 __all__ = (new_random_password, encrypt)
-__version__ = "0.1alpha"
+__version__ = "0.0.5"
 __author__ = "Ayush K M"
 
 if __name__ == "__main__":
