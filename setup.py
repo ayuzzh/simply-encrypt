1e54a85e661bf5131a50bfe9d068ed89797d5aac
Ayush K M <kmayushkm@gmail.com>
2021-09-05 14:10:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Simple Encryption Tool'
 LONG_DESCRIPTION = 'A package that can be used to do simple encryption of strings with random passwords.'
 
